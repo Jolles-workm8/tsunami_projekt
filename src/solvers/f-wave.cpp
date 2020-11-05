f3f66c6e598a3bb893eccef09ad062f2e811c8bd
Julius Isken <julius.isken@googlemail.com>
2020-11-05 14:16:52 +0100

    solver files

---
+++ src/solvers/f-wave.cpp
@@ -0,0 +1,6 @@
+#include "f-wave.h"
+#include <cmath>
+
+void tsunami_lab::solvers::f -
+    wave::waveSpeeds(t_real i_hL, t_real i_hR, t_real i_uL, t_real i_uR,
+                     t_real &o_waveSpeedL, t_real &o_waveSpeedR) {}
