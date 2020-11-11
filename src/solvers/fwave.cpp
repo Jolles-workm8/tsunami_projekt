ac49ed45f4134052ea9245e6689f053f42d0e19e
Julius Isken <julius.isken@googlemail.com>
2020-11-11 17:01:24 +0100

    still debugging

--- src/solvers/fwave.cpp
+++ src/solvers/fwave.cpp
@@ -1,84 +1,130 @@
+/**
+ * @author Alexander Breuer (alex.breuer AT uni-jena.de)
+ *
+ * @section LICENSE
+ * Copyright 2020, Friedrich Schiller University Jena
+ *
+ * Redistribution and use in source and binary forms, with or without
+ *modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *this list of conditions and the following disclaimer in the documentation
+ *and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @section DESCRIPTION
+ * Roe Riemann solver for the shallow water equations.
+ **/
 #include "fwave.h"
 #include <cmath>
 
+// compute the lambdas we need
+void tsunami_lab::solvers::fwave::waveSpeeds(t_real i_hL, t_real i_hR,
+                                             t_real i_uL, t_real i_uR,
+                                             t_real &o_waveSpeedL,
+                                             t_real &o_waveSpeedR) {
+  // pre-compute square-root ops
+  t_real l_hSqrtL = std::sqrt(i_hL);
+  t_real l_hSqrtR = std::sqrt(i_hR);
 
-void tsunami_lab::solvers::fwave::getLambda(t_real i_hL, t_real i_hR,
-                                          t_real i_huL, t_real i_huR,
-                                          t_real &lambda){
+  // compute Roe averages
+  t_real l_hRoe = 0.5f * (i_hL + i_hR);
+  t_real l_uRoe = l_hSqrtL * i_uL + l_hSqrtR * i_uR;
+  l_uRoe /= l_hSqrtL + l_hSqrtR;
 
-  // prepare the variables
-  t_real i_uL = i_huL / i_hL;
-  t_real i_uR = i_huR / i_hR;
-
-  // precompute square roots
-  t_real hl_sqrt = std::sqrt(i_hL);
-  t_real hr_sqrt = std::sqrt(i_hR);
-
-  // precompute h_roe and u_roe
-  t_real h_roe_sqrt = std::sqrt((i_hL + i_hR) / 2);
-  t_real u_roe = (i_uL * hl_sqrt + i_uR * hr_sqrt) / (hr_sqrt + hl_sqrt);
-
-  // compute the lambdas using array to align the memory for parallelization
-  // later
-  lambda[0] = u_roe - (m_g Sqrt * h_roe_sqrt);
-  lambda[1] = u_roe + (m_gSqrt * h_roe_sqrt);
+  // compute wave speeds
+  t_real l_ghSqrtRoe = m_gSqrt * std::sqrt(l_hRoe);
+  o_waveSpeedL = l_uRoe - l_ghSqrtRoe;
+  o_waveSpeedR = l_uRoe + l_ghSqrtRoe;
 }
 
-void tsunami_lab::solvers::fwave::zsolver(t_real i_hL, t_real i_hR,
-                                          t_real i_huL, t_real i_huR,
-                                          t_real lambda,
-                                          t_real &z_1, t_real &z_2) {
+// compute the inverse of the matrix
+void tsunami_lab::solvers::fwave::waveStrengths(
+    t_real i_hL, t_real i_hR, t_real i_huL, t_real i_huR, t_real i_waveSpeedL,
+    t_real i_waveSpeedR, t_real &o_strengthL, t_real &o_strengthR) {
+  // compute inverse of right eigenvector-matrix
+  t_real l_detInv = 1 / (i_waveSpeedR - i_waveSpeedL);
 
-  // precompute the factors that can be precomputed, hard square is faster then
-  // casting power in C++
-  t_real lambda_factor = 1 / (lambda[1] - lambda[0]);
-  t_real df1 = i_huR - i_huL;
-  t_real df2 =
-      i_huR * i_huR - i_huL * i_huL + g / 2 * (i_hR * i_hR - i_hL*i_hL);
+  t_real l_rInv[2][2] = {0};
+  l_rInv[0][0] = l_detInv * i_waveSpeedR;
+  l_rInv[0][1] = -l_detInv;
+  l_rInv[1][0] = -l_detInv * i_waveSpeedL;
+  l_rInv[1][1] = l_detInv;
 
-  // compute the alpha
-  t_real alpha_1 = lambda_factor * (lambda[1] * df1 - df2);
-  t_real alpha_2 = lambda_factor * (df2 - lambda[0] * df1);
+  // compute jump in quantities
+  t_real l_hJump = i_huR - i_huL;
+  t_real l_huJump =
+      i_huR * i_huR - i_huL * i_huL + (g / 2) * (i_hR * i_hR - i_hL * i_hL);
 
-  // compute the z's, again array to align memory, for formula see
-  // documentation
-  z_1[0] = alpha_1;
-  z_1[1] = alpha_1 * lambda[0];
+  // compute wave strengths
+  o_strengthL = l_rInv[0][0] * l_hJump;
+  o_strengthL += l_rInv[0][1] * l_huJump;
 
-  z_2[0] = alpha_2;
-  z_2[1] = alpha_2 * lambda[1];
+  o_strengthR = l_rInv[1][0] * l_hJump;
+  o_strengthR += l_rInv[1][1] * l_huJump;
 }
 
 void tsunami_lab::solvers::fwave::netUpdates(t_real i_hL, t_real i_hR,
                                              t_real i_huL, t_real i_huR,
                                              t_real o_netUpdateL[2],
                                              t_real o_netUpdateR[2]) {
+  // compute particle velocities
+  t_real l_uL = i_huL / i_hL;
+  t_real l_uR = i_huR / i_hR;
 
-  // set memory for lambda and z.
+  // compute wave speeds
+  t_real l_sL = 0;
+  t_real l_sR = 0;
 
-  t_real lambda[2];
-  t_real z_1[2];
-  t_real z_2[2];
+  waveSpeeds(i_hL, i_hR, l_uL, l_uR, l_sL, l_sR);
 
-  //compute the Eigenvalues Lambda
-  getLambda(i_hL, i_hR, i_huL, i_huR, lambda);
+  // compute wave strengths
+  t_real l_aL = 0;
+  t_real l_aR = 0;
 
-  // compute the z's
-  zsolver(i_hL, i_hR, i_huL, i_huR, lambda, z_1, z_2);
+  waveStrengths(i_hL, i_hR, i_huL, i_huR, l_sL, l_sR, l_aL, l_aR);
 
-  if (lambda[0] < 0) {
-    o_netUpdateL[0] += z_1[0];
-    o_netUpdateL[1] += z_1[1];
-  } else {
-    o_netUpdateR[0] += z_1[0];
-    o_netUpdateR[1] += z_1[1];
-  }
+  // compute scaled waves
+  t_real l_waveL[2] = {0};
+  t_real l_waveR[2] = {0};
 
-  if (lambda[1] < 0) {
-    o_netUpdateL[0] += z_2[0];
-    o_netUpdateL[1] += z_2[1];
-  } else {
-    o_netUpdateR[0] += z_2[0];
-    o_netUpdateR[1] += z_2[1];
+  l_waveL[0] = l_sL * l_aL;
+  l_waveL[1] = l_sL * l_aL * l_sL;
+
+  l_waveR[0] = l_sR * l_aR;
+  l_waveR[1] = l_sR * l_aR * l_sR;
+
+  // set net-updates depending on wave speeds
+  for (unsigned short l_qt = 0; l_qt < 2; l_qt++) {
+    // init
+    o_netUpdateL[l_qt] = 0;
+    o_netUpdateR[l_qt] = 0;
+
+    // 1st wave
+    if (l_sL < 0) {
+      o_netUpdateL[l_qt] = l_waveL[l_qt];
+    } else {
+      o_netUpdateR[l_qt] = l_waveL[l_qt];
+    }
+
+    // 2nd wave
+    if (l_sR > 0) {
+      o_netUpdateR[l_qt] = l_waveR[l_qt];
+    } else {
+      o_netUpdateL[l_qt] = l_waveR[l_qt];
+    }
   }
 }
--- src/solvers/fwave.test.cpp
+++ src/solvers/fwave.test.cpp
@@ -3,40 +3,31 @@
 #include "fwave.h"
 #undef public
 
-TEST_CASE("Test Eigenvalues lambda", "[lambdas]"){
-/*
-* Test case:
-*  h: 10 | 9
-*  hu: -30 | 27
-*  u: -3| 3
-*
-* roe height: 9.5
-* roe velocity: (sqrt(10) * -3 + 3 * 3) / ( sqrt(10) + sqrt(9) )
-* = -0.07900211696917202
-* sqrt(g*height): 3.131557121*sqrt(9.5) =  9.652107283894807
-*
-* lambda[0]: -0.07900211696917202 - 9.652107283894807 = -9.731109400863979
-* lambda[1]: -0.07900211696917202 + 9.652107283894807 =  9.573105166925635
-*
-*
-*/
-
+TEST_CASE("Test Eigenvalues lambda", "[lambdas]") {
+  /*
+   * Test case:
+   *  h: 10 | 9
+   *  hu: -30 | 27
+   *  u: -3| 3
+   *
+   * roe height: 9.5
+   * roe velocity: (sqrt(10) * -3 + 3 * 3) / ( sqrt(10) + sqrt(9) )
+   * = -0.07900211696917202
+   * sqrt(g*height): 3.131557121*sqrt(9.5) =  9.652107283894807
+   *
+   * lambda[0]: -0.07900211696917202 - 9.652107283894807 = -9.731109400863979
+   * lambda[1]: -0.07900211696917202 + 9.652107283894807 =  9.573105166925635
+   *
+   *
+   */
 
   float lambda[2];
   lambda[0] = 0;
   lambda[1] = 0;
-  tsunami_lab::solvers::fwave::getLambda(10,
-                                         9,
-                                         -3,
-                                         3,
-                                         lambda);
+  tsunami_lab::solvers::fwave::getLambda(10, 9, -3, 3, lambda);
 
-  REQUIRE( lambda[0] == Approx( -9.731109400863979 ) );
-  REQUIRE( lambda[1] == Approx(  9.573105166925635 ) );
-
+  REQUIRE(lambda[0] == Approx(-9.731109400863979));
+  REQUIRE(lambda[1] == Approx(9.573105166925635));
 }
 
-
-TEST_CASE("Test steady states", "[steadyState]"){
-
-}
+TEST_CASE("Test steady states", "[steadyState]") {}
