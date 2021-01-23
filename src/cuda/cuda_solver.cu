bd2fc8a1109345c86abd355e69fc9a1147bf5d75
Julius Isken <>
2021-01-23 14:37:15 +0100

    waveprop

--- src/cuda/cuda_solver.cu
+++ src/cuda/cuda_solver.cu
@@ -1,42 +1,32 @@
 #include <cuda.h>
-#include <thrust\device_vector.h>
 
 #include <iostream>
 
-__global__ void netUpdates(float *i_h_old, float *i_h_new, float *i_hu_old,
-                           float *i_hu_new, int i_nx, int i_ny, float *i_b) {
+__device__ void netUpdates(float *i_height_old, float *i_height_new,
+                           float *i_momentum_old, float *i_momentum_new,
+                           int i_nx, int i_ny, float *i_b, float i_scaling) {
   int scaling = 4;
   int l_i = blockIdx.x * blockDim.x + threadIdx.x;
   int l_j = blockIdx.y * blockDim.y + threadIdx.y;
-  int tid = threadIdx.x + threadIdx.y * blockDim.x;
   int idx = l_i * scaling + l_j;
 
   int i_displ = 1;
   float m_gSqrt = sqrtf(9.812);
   float m_g = 9.812;
 
-  i_h_new[idx] = 0;
-
-  printf("%f\n %f", i_h_new[idx], i_h_old[idx]);
-  printf("%d\n", idx);
-
-  if (l_i < i_nx && l_j < i_ny) {
-    printf("%d, %d\n", l_i, l_j);
-
+  if (l_i < i_nx - 1 && l_j < i_ny - 1) {
     // compute u for left and right
-    float l_uL = i_hu_old[idx] / i_h_old[idx];
-    float l_uR = i_hu_old[idx + i_displ] / i_h_old[idx + i_displ];
+    float l_uL = i_momentum_old[idx] / i_height_old[idx];
+    float l_uR = i_momentum_old[idx + i_displ] / i_height_old[idx + i_displ];
 
-    float l_hL = i_h_old[idx];
-    float l_hR = i_h_old[idx + i_displ];
+    float l_hL = i_height_old[idx];
+    float l_hR = i_height_old[idx + i_displ];
 
-    float l_huL = i_hu_old[idx];
-    float l_huR = i_hu_old[idx + i_displ];
+    float l_huL = i_momentum_old[idx];
+    float l_huR = i_momentum_old[idx + i_displ];
 
     float l_bL = i_b[idx];
     float l_bR = i_b[idx + i_displ];
-
-    __syncthreads();
 
     // compute WaveSpeed ,
 
@@ -48,8 +38,6 @@ __global__ void netUpdates(float *i_h_old, float *i_h_new, float *i_hu_old,
     l_uRoe /= l_hSqrtL + l_hSqrtR;
 
     float l_ghSqrtRoe = m_gSqrt * sqrtf(l_hRoe);
-
-    __syncthreads();
 
     float l_waveSpeedL = l_uRoe - l_ghSqrtRoe;
     float l_waveSpeedR = l_uRoe + l_ghSqrtRoe;
@@ -66,70 +54,93 @@ __global__ void netUpdates(float *i_h_old, float *i_h_new, float *i_hu_old,
     l_fJump_2 -= l_bathEff;
 
     // compute the alpha values
-    float l_strengthL = l_detInv * (l_waveSpeedR * l_fJump_1 - l_fJump_2);
-    float l_strengthR = l_detInv * (l_fJump_2 - l_waveSpeedL * l_fJump_1);
+    float l_strengthL =
+        -i_scaling * l_detInv * (l_waveSpeedR * l_fJump_1 - l_fJump_2);
+    float l_strengthR =
+        -i_scaling * l_detInv * (l_fJump_2 - l_waveSpeedL * l_fJump_1);
 
-    printf("Hello world  %f\n", l_waveSpeedL);
     __syncthreads();
 
     if (l_waveSpeedL < 0) {
-      atomicAdd(&i_h_new[idx], l_strengthL);
-      atomicAdd(&i_hu_new[idx], l_strengthL * l_waveSpeedL);
+      atomicAdd(&i_height_new[idx], l_strengthL);
+      atomicAdd(&i_momentum_new[idx], l_strengthL * l_waveSpeedL);
     } else {
-      atomicAdd(&i_h_new[idx + i_displ], l_strengthL);
-      atomicAdd(&i_hu_new[idx + i_displ], l_strengthL * l_waveSpeedL);
+      atomicAdd(&i_height_new[idx + i_displ], l_strengthL);
+      atomicAdd(&i_momentum_new[idx + i_displ], l_strengthL * l_waveSpeedL);
     }
 
     if (l_waveSpeedR > 0) {
-      atomicAdd(&i_h_new[idx + i_displ], l_strengthR);
-      atomicAdd(&i_hu_new[idx + i_displ], l_strengthR * l_waveSpeedR);
+      atomicAdd(&i_height_new[idx + i_displ], l_strengthR);
+      atomicAdd(&i_momentum_new[idx + i_displ], l_strengthR * l_waveSpeedR);
     } else {
-      atomicAdd(&i_h_new[idx], l_strengthR);
-      atomicAdd(&i_hu_new[idx], l_strengthR * l_waveSpeedR);
+      atomicAdd(&i_height_new[idx], l_strengthR);
+      atomicAdd(&i_momentum_new[idx], l_strengthR * l_waveSpeedR);
     }
     __syncthreads();
 
-    printf("%f\n", i_h_new[idx]);
+    i_height_old[idx] = i_height_new[idx];
+    i_momentum_old[idx] = i_momentum_new[idx];
+  }
+}
 
-    swap(i_h_new, i_h_old);
-
-    printf("%f\n", i_h_new[idx]);
+__global__ void WavePropagation2d(float *i_h_old, float *i_h_new,
+                                  float *i_hu_old, float *i_hu_new,
+                                  float *i_hv_new, float *i_hv_old, int i_nx,
+                                  int i_ny, float *i_b, int i_iter,
+                                  float i_scaling) {
+  for (int i = 0; i < i_iter; ++i) {
+    netUpdates(i_h_old, i_h_new, i_hu_old, i_hu_new, i_nx, i_ny, i_b,
+               i_scaling);
+    __syncthreads();
   }
 }
 
 int main() {
+  int N = 128;
+  int iteration = 32;
   int size = 16;
   int i_nx = 4;
   int i_ny = 4;
-  float *h_host, *hu_host, *h_dev_new, *h_dev_old, *hu_dev_new, *hu_dev_old,
-      *b_host, *b_dev;
+  float scaling = 0.5;
+  float *h_host, *hu_host, *hv_host, *h_dev_new, *h_dev_old, *hu_dev_new,
+      *hu_dev_old, *hv_dev_new, *hv_dev_old, *b_host, *b_dev;
   h_host = (float *)malloc(size * sizeof(float));
+  hu_host = (float *)malloc(size * sizeof(float));
   hu_host = (float *)malloc(size * sizeof(float));
   b_host = (float *)malloc(size * sizeof(float));
   cudaMalloc((void **)&h_dev_old, size * sizeof(float));
   cudaMalloc((void **)&h_dev_new, size * sizeof(float));
   cudaMalloc((void **)&hu_dev_old, size * sizeof(float));
   cudaMalloc((void **)&hu_dev_new, size * sizeof(float));
+  cudaMalloc((void **)&hv_dev_old, size * sizeof(float));
+  cudaMalloc((void **)&hv_dev_new, size * sizeof(float));
   cudaMalloc((void **)&b_dev, size * sizeof(float));
 
   for (int i = 0; i < size; i++) {
     h_host[i] = i;
     hu_host[i] = i;
+    hv_host[i] = i;
     b_host[i] = 0;
   }
 
   cudaMemcpy(h_dev_old, h_host, size * sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(hu_dev_old, hu_host, size * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(hv_dev_old, hv_host, size * sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(b_dev, b_host, size * sizeof(float), cudaMemcpyHostToDevice);
 
   cudaError_t err = cudaGetLastError();
-  dim3 threadsPerBlock(4, 4);
+  dim3 threadsPerBlock(i_nx, i_ny);
+  for (int i = 0; i < N / iteration; i++) {
+    WavePropagation2d<<<1, threadsPerBlock>>>(
+        h_dev_old, h_dev_new, hu_dev_old, hu_dev_new, hv_dev_new, hv_dev_old,
+        i_nx, i_ny, b_dev, iteration, scaling);
 
-  netUpdates<<<1, threadsPerBlock>>>(h_dev_old, h_dev_new, hu_dev_old,
-                                     hu_dev_new, i_nx, i_ny, b_dev);
-
+    cudaDeviceSynchronize();
+  }
   cudaMemcpy(h_host, h_dev_old, size * sizeof(float), cudaMemcpyDeviceToHost);
   cudaMemcpy(hu_host, hu_dev_old, size * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(hv_host, hv_dev_old, size * sizeof(float), cudaMemcpyDeviceToHost);
+
   //
   // for (int i = 0; i < size; i++) {
   //   std::cout << h_host[i] << '\n';
